9d65465847a7b7def5cdccd2012761758317d9a3
Adrian Kozłowski <adrian.kozlowski2611@interia.pl>
2020-10-08 13:24:09 +0200

    Added eager initialization implementation of the Singleton design pattern

---
+++ Singleton/src/main/java/com/akozlowski/eagerinitialization/Client.java
@@ -0,0 +1,24 @@
+package com.akozlowski.eagerinitialization;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client {
+
+    private static final Logger logger = LoggerFactory.getLogger(Client.class);
+
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+
+        singleton.setData(55);
+
+        logger.info("First reference is: {}", singleton);
+        logger.info("Singleton data value is: {} ", singleton.getData());
+
+        singleton = null;
+        singleton = Singleton.getInstance();
+
+        logger.info("First reference is: {}", singleton);
+        logger.info("Singleton data value is: {} ", singleton.getData());
+    }
+}
